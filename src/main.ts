933f0c9b2034a883c9245674570e38bc5809bad2
âBrank-Lan <2055929044@qq.com>
2021-04-02 16:28:27 +0800

    搭建HTML结构

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import {createApp} from 'vue';
 import App from './App.vue';
 import './index.css';
 import {createWebHashHistory, createRouter} from 'vue-router';
-import Frank from './components/Frank.vue';
-import Frank2 from './components/Frank2.vue';
+import Home from './view/Home.vue';
+import Doc from './view/Doc.vue';
 
 const history = createWebHashHistory();
 const router = createRouter({
   history: history,
   routes: [
-    {path: '/', component: Frank},
-    {path: '/Frank2', component: Frank2}
+    {path: '/', component: Home},
+    {path: '/Frank2', component: Doc}
   ]
 });
 const app = createApp(App);
---
+++ src/view/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>轱辘ui</h1>
+      <h2>一个厉害的UI框架</h2>
+      <p class="action">
+        <a>GitHub</a>
+        <a>开始</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
