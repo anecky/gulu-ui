0e78dfc5414cad7b9e3d2946dc8b81b13f123676
âBrank-Lan <2055929044@qq.com>
2021-04-03 01:28:15 +0800

    aside的html

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -4,9 +4,9 @@
     <div class="banner">
       <h1>轱辘ui</h1>
       <h2>一个厉害的UI框架</h2>
-      <p class="action">
-        <a>GitHub</a>
-        <a>开始</a>
+      <p class="actions">
+        <a href="https://github.com/LanX-Lan">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
