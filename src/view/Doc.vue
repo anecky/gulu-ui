4af90c1c32311b788d035a3262955ab880ddf3de
âBrank-Lan <2055929044@qq.com>
2021-04-03 01:31:52 +0800

    aside的css1

--- src/view/Doc.vue
+++ src/view/Doc.vue
@@ -25,14 +25,29 @@
 </template>
 
 <script lang="ts">
-  import Topnav from '../components/Topnav.vue'
+  import Topnav from '../components/Topnav.vue';
 
   export default {
     name: 'Doc',
     components: {Topnav}
-  }
+  };
 </script>
 
 <style lang="scss" scoped>
-
+  aside {
+    background: lightblue;
+    width: 150px;
+    padding:16px;
+    position: fixed;
+    top:0;
+    left: 0;
+    > h2 {
+      margin-bottom:4px;
+    }
+    > ol{
+      >li{
+        padding:4px 0;
+      }
+    }
+  }
 </style>
