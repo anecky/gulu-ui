d052f84c46f362dd37337242866f2c292faece13
âBrank-Lan <2055929044@qq.com>
2021-04-02 16:04:56 +0800

    添加tsconfig.json

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue';
 import './index.css';
 import {createWebHashHistory, createRouter} from 'vue-router';
 import Frank from './components/Frank.vue';
-import Frank2 from './components/Frank2';
+import Frank2 from './components/Frank2.vue';
 
 const history = createWebHashHistory();
 const router = createRouter({
