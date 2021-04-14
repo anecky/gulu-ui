9479530a2dc2fcf594596a65ad9d14a11ace58bd
âBrank-Lan <2055929044@qq.com>
2021-04-14 12:36:47 +0800

    创建dialog css1

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="dialog-overlay"></div>
-  <div class="dialog-wrapper">
-    <header>标题</header>
-    <main>
-      <p>第一行</p>
-      <p>第二行</p>
-    </main>
-    <footer>
-      <Button level="main">ok</Button>
-      <Button>cancel</Button>
-    </footer>
+  <div class="gulu-dialog-overlay"></div>
+  <div class="gulu-dialog-wrapper">
+    <div class="gulu-dialog">
+      <header>标题<span class="gulu-dialog-close"></span></header>
+      <main>
+        <p>第一行</p>
+        <p>第二行</p>
+      </main>
+      <footer>
+        <Button level="main">ok</Button>
+        <Button>cancel</Button>
+      </footer>
+    </div>
   </div>
 </template>
 
@@ -23,5 +25,85 @@
 </script>
 
 <style lang="scss" scoped>
+  $radius: 4px;
+  $border-color: #d9d9d9;
+  .gulu-dialog {
+    background: white;
+    border-radius: $radius;
+    box-shadow: 0 0 3px fade_out(black, 0.5);
+    min-width: 15em;
+    max-width: 90%;
 
+    &-overlay {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: fade_out(black, 0.5);
+      z-index: 10;
+    }
+
+    &-wrapper {
+      position: fixed;
+      left: 50%;
+      top: 50%;
+      -webkit-transform: translate(-50%, -50%);
+      -moz-transform: translate(-50%, -50%);
+      -ms-transform: translate(-50%, -50%);
+      -o-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+      z-index: 11;
+    }
+
+    > header {
+      padding: 12px 16px;
+      border-bottom: 1px solid $border-color;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      font-size: 20px;
+    }
+
+    > main {
+      padding: 12px 16px;
+    }
+
+    > footer {
+      border-top: 1px solid $border-color;
+      padding: 12px 16px;
+      text-align: right;
+    }
+
+    &-close {
+      position: relative;
+      display: inline-block;
+      width: 16px;
+      height: 16px;
+      cursor: pointer;
+
+      &::before,
+      &::after {
+        content: '';
+        position: absolute;
+        height: 1px;
+        background: black;
+        width: 100%;
+        top: 50%;
+        left: 50%;
+      }
+
+      &::before {
+        transform: translate(-50%, -50%) rotate(-45deg);
+      }
+
+      &::after {
+        -webkit-transform: translate(-50%, -50%) rotate(45deg);
+        -moz-transform: translate(-50%, -50%) rotate(45deg);
+        -ms-transform: translate(-50%, -50%) rotate(45deg);
+        -o-transform: translate(-50%, -50%) rotate(45deg);
+        transform: translate(-50%, -50%) rotate(45deg);
+      }
+    }
+  }
 </style>
