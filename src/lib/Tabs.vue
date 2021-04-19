201d60ce7a3253f750459583e144ddbca03a4afc
âBrank-Lan <2055929044@qq.com>
2021-04-19 13:33:05 +0800

    使用css进行内容切换

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="gulu-tabs">
     <div class="gulu-tabs-nav">
-      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+      <div class="gulu-tabs-nav-item"
+           v-for="(t,index) in titles"
+           @click="select(t)"
+           :class="{selected:t===selected}"
+           :key="index">{{t}}
+      </div>
     </div>
     <div class="gulu-tabs-content">
-      <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index"/>
+      <component class="gulu-tabs-content-item"
+                 v-for="c in defaults"
+                 :class="{selected: c.props.title === selected}"
+                 :is="c"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
   import Tab from './Tab.vue';
+  import {computed} from 'vue';
 
   export default {
     name: 'Tabs',
+    props: {
+      selected: {
+        type: String
+      }
+    },
     setup(props, context) {
       const defaults = context.slots.default();
       defaults.forEach((tag) => {
@@ -21,15 +35,24 @@
           throw new Error('Tabs 子标签必须是Tab');
         }
       });
+      const current = computed(() => {
+        console.log('重新 return');
+        return defaults.filter((tag) => {
+          return tag.props.title === props.selected;
+        })[0];
+      });
+      const select = (title: string) => {
+        context.emit('update:selected', title);
+      };
       const titles = defaults.map(tag => {
         return tag.props.title;
       });
-      return {defaults, titles};
+      return {defaults, titles, select, current};
     }
   };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   $blue: #40a9ff;
   $color: #333;
   $border-color: #d9d9d9;
@@ -57,6 +80,17 @@
 
     &-content {
       padding: 8px 0;
+      &-item {
+        border: 1px solid red;
+        display: none;
+
+        &.selected {
+          border: 1px solid green;
+          display: block;
+        }
+      }
     }
   }
+
+
 </style>
