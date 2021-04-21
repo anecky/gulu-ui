71d32f7d7376f15044d7e548f87eebc72f23b1b0
âBrank-Lan <2055929044@qq.com>
2021-04-21 20:58:09 +0800

    首页背景颜色渐变

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="topnavAndBanner">
     <Topnav/>
     <div class="banner">
       <h1>轱辘ui</h1>
@@ -13,22 +13,25 @@
 </template>
 
 <script lang="ts">
-  import Topnav from '../components/Topnav.vue'
+  import Topnav from '../components/Topnav.vue';
 
   export default {
     name: 'Home',
     components: {Topnav}
-  }
+  };
 </script>
 
 <style lang="scss" scoped>
+  .topnavAndBanner {
+    background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+  }
+
   .banner {
     padding: 100px 0;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    background: lightgreen;
 
     > .actions {
       padding: 8px 0;
