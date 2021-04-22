44c25e0a1faebda2b664e38547a3372404272afd
âBrank-Lan <2055929044@qq.com>
2021-04-22 22:58:08 +0800

    选中的路由,复杂一点的高亮

--- src/view/Doc.vue
+++ src/view/Doc.vue
@@ -3,7 +3,7 @@
     <Topnav toggle-menu-button-visible class="nav"/>
     <div class="content">
       <aside v-if="menuVisible">
-        <h2>组件列表</h2>
+        <h2>文档</h2>
         <ol>
           <li>
             <router-link to="/doc/intro">介绍</router-link>
@@ -14,6 +14,9 @@
           <li>
             <router-link to="/doc/install">安装</router-link>
           </li>
+        </ol>
+        <h2>组件列表</h2>
+        <ol>
           <li>
             <router-link to="/doc/switch">Switch 组件</router-link>
           </li>
@@ -91,19 +94,24 @@
     position: fixed;
     top: 0;
     left: 0;
-    padding: 70px 16px 16px;
+    padding-top: 70px;
     height: 100%;
 
     > h2 {
+      padding: 0 16px;
       margin-bottom: 4px;
     }
 
     > ol {
       > li {
-        padding: 4px 0;
+        > a {
+          display: block;
+          text-decoration: none;
+          padding: 4px 16px;
+        }
 
         .router-link-active {
-          color: red;
+          background: #ffffff;
         }
       }
     }
