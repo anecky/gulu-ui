9137a45bd96a604dd901b11aa565bbcc77fe23d4
âBrank-Lan <2055929044@qq.com>
2021-04-23 00:17:31 +0800

    填充文档内容

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-
 import './index.scss';
 import './lib/gulu.scss';
 import {createApp} from 'vue';
 import App from './App.vue';
 import router from './router';
+import 'github-markdown-css';
 
 const app = createApp(App);
 app.use(router);
