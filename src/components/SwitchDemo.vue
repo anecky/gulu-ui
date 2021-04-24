4250eb1a2a9136dd728141639e3398021bcf807e
âBrank-Lan <2055929044@qq.com>
2021-04-24 19:56:45 +0800

    抽离用户需要拷贝的代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,17 +1,41 @@
 <template>
-  <div>
-    <Switch v-model:value="value"/>
+  <div class="demo">
+    <h2>常规用法</h2>
+    <div class="demo-component">
+      <SwitchDemo1/>
+    </div>
+    <div class="demo-action">
+      <Button>查看代码</Button>
+    </div>
+    <div class="demo-code">
+      <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+      <pre>&lt;Switch v-model:value="bool" disable /&gt;</pre>
+    </div>
   </div>
-
+  <div class="demo">
+    <h2>支持 disabled</h2>
+    <div class="demo-component">
+      <SwitchDemo2/>
+    </div>
+    <div class="demo-action">
+      <Button>查看代码</Button>
+    </div>
+    <div class="demo-code">
+      <pre>&lt;Switch v-model:value="bool" disable /&gt;</pre>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
   import Switch from '../lib/Switch.vue';
   import {ref} from 'vue';
+  import SwitchDemo1 from './switchDemo/SwitchDemo1.vue';
+  import Button from '../lib/Button.vue';
+  import SwitchDemo2 from './switchDemo/SwitchDemo2.vue';
 
   export default {
     name: 'SwitchDemo',
-    components: {Switch},
+    components: {SwitchDemo2, Button, SwitchDemo1, Switch},
     setup() {
       const value = ref(false);
       return {value};
@@ -20,5 +44,35 @@
 </script>
 
 <style lang="scss" scoped>
+  $border-color: #d9d9d9;
+  .demo {
+    border: 1px solid $border-color;
+    margin: 16px 0 32px;
 
+    > h2 {
+      font-size: 20px;
+      padding: 8px 16px;
+      border-bottom: 1px solid $border-color;
+    }
+
+    &-component {
+      padding: 16px;
+    }
+
+    &-actions {
+      padding: 8px 16px;
+      border-top: 1px dashed $border-color;
+    }
+
+    &-code {
+      padding: 8px 16px;
+      border-top: 1px dashed $border-color;
+
+      > pre {
+        line-height: 1.1;
+        font-family: Consolas, 'Courier New', Courier, monospace;
+        margin: 0;
+      }
+    }
+  }
 </style>
